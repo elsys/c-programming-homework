e7fb71ecbee75efdf4bfe98b8797f7620936e0a8
Peter Mihaylov <peps.mihailov@gmail.com>
2016-10-12 23:48:25 +0300

    Update task04.c

--- B/02/19/task04.c
+++ B/02/19/task04.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+char opposite_char(char);
 int main()
 {
  char c;
- do{
  scanf("%c", &c);
- }while(!((c >= 'A' && c <= 'Z') || (c >= 'a' &&  c <= 'z')));
- c = ((char)((int) c + 13));
- printf("%c\n", c);
+ printf("%c\n", opposite_char(c));
+
  return 0;
+}
+char opposite_char(char c)
+{
+  if((c >= 'a') && (c <= 'z'))
+  {
+    int middle = ('a' +(('z' - 'a')/2);
+    char opposite = middle - c + middle + 1;
+    return opposite;
+  }
+
+  if((c >= 'A') && (c <= 'Z'))
+  {
+    int middle = ('A' +(('Z' - 'A')/2);
+    char opposite = middle - c + middle + 1;
+    return opposite;
+  }
 }
