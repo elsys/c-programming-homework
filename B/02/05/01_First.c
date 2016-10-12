f92e89f33fb736c3c43ee0b2470a5137480405d9
Vladislav <vladdislav.georgiev@gmail.com>
2016-10-12 15:22:29 -0700

    Homework

---
+++ B/02/05/01_First.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	int n;
+	int b;
+	int t;
+	  scanf("%d",&n);
+	b=n*n;
+	t=n*n*n;
+	printf("%d\n", b);
+	printf("%d", t);
+	}
---
+++ B/02/05/02_Second.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() 
+
+	{
+	  float a;
+	  float b;
+	  float c;
+	scanf("%f",&a);
+	scanf("%f",&b);
+	scanf("%f",&c);
+          float max=a;
+	
+	if(max<b)
+	{
+	 max=b;
+	      if(max<c)
+		{
+		 max=c;
+		} 
+	}
+	else if(max<c){
+	max=c;
+	};
+	printf("%.1f",max);
+	}
