cd9a27fe3da10f41589f4f114d407228cad70af2
mimibeleva <mimibelev@gmail.com>
2016-10-12 21:36:46 +0300

    Create 02_domachno.c

---
+++ G/02/19/02_domachno.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{   float a,b,c;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    if(a>b)if(a>c)printf("%f \n",a);
+           else printf("%f \n",c);
+    else if(b>c)printf("%f \n",b);
+         else printf("%f \n",c);
+}
