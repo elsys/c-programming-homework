7dd8990cb576ce4263997e4bd016077aeab7a9fe
Wrobel8 <malinov.marti@gmail.com>
2016-10-13 12:59:50 -0700

    Martin Malinov 10v

---
+++ V/02/17/02_10v_17_task03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+	int a;
+	printf("\n Input number >");
+	scanf("%d", &a);
+	switch(a) {
+		case 1: printf("\nOne");break;
+		case 2: printf("\nTwo");break;
+		case 3: printf("\nThree");break;
+		case 4: printf("\nFour");break;
+		case 5: printf("\nFive");break;
+		case 6: printf("\nSix");break;
+		case 7: printf("\nSeven");break;
+		case 8: printf("\nEight");break;
+		case 9: printf("\nNine");break;
+	}
+}
