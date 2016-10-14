34b5e660e347594ad9ea9e487aed54181ea24040
Boril Sarbev <b.sarbev@gmail.com>
2016-10-14 10:26:17 +0300

    Boril Sarbev 10v

---
+++ V/02/05/02_task01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int a,sq,cb;
+	
+	printf("\nEnter a number:");
+	scanf("%d",&a);
+
+	sq=a*a;
+	cb=a*a*a;
+
+	printf("\n Square:%d",sq);
+	printf("\n Cube:%d",cb);
+
+
+}
---
+++ V/02/05/02_task03.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+
+	printf("\nEnter a number:");
+        scanf("%d",&a);
+	if(a>=0&&a<=9)
+	{	
+	if(a==0)
+	{
+	printf("\nZero");
+	}
+	if(a==1)
+	{
+	printf("\nOne");
+	}
+	if(a==2)
+	{
+	printf("\nTwo");
+	}
+	if(a==3)
+	{
+	printf("Three");
+	}
+	if(a==4)
+	{
+	printf("Four");
+	}
+	if(a==5)
+	{
+	printf("Five");
+	}
+	if(a==6)
+	{
+	printf("Six");
+	}
+	if(a==7)
+	{
+	printf("Seven");
+	}
+	if(a==8)
+	{
+	printf("Eight");
+	}
+	if(a==9)
+	{
+	printf("Nine");
+	}
+	}
+return 0;
+
+}
