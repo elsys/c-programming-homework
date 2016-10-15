0f6484c216cd8079b2c1990fbd980561c89fa228
MartinPetrovWasTaken <martin033@abv.bg>
2016-10-15 22:32:11 +0300

    Update task04.c

--- A/02/15/task04.c
+++ A/02/15/task04.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
-
 int main(){
   char a, opp;
-
   scanf("%c", &a);
   opp = ('z' - (a - 'a'));
-  printf("The opposite is:\n%c\n", opp);
+  printf("\n%c\n", opp);
   return 0;
 }
