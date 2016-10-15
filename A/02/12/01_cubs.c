b75b2fcd041a3d329b5d5c850605eb0622a1ca4d
karinakozarova <#include stdio.h>
2016-10-15 21:33:35 +0300

    Second HW

---
+++ A/02/12/01_cubs.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+  int input,kvadratwe,kubwe;
+  scanf("%d",&input);
+  kvadratwe=input*input;
+  kubwe=kvadratwe*input;
+  printf("%d\n%d\n",kvadratwe,kubwe);
+}
