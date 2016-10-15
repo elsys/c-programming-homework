1eb91138feb1b37373fca33ffc0d9315b45ebcb8
viktorgg <viktor.g.g@abv.bg>
2016-10-15 19:55:43 +0300

    Create task02.c

---
+++ A/02/05/task02.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   float a, b, c;
+   scanf("%f",&a);
+   scanf("%f",&b);
+   scanf("%f",&c);
+
+   if(a>b && a>c){
+    printf("%f\n",a);
+   }
+   if(b>a && b>c){
+    printf("%f\n",b);
+   }
+   if(c>a && c>b){
+    printf("%f\n",c);
+   }
+}
