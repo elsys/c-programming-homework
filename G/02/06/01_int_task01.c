341ddced4753e5b3c5b72ac101733b7f65f30b17
VKanevv <vesko.kynev@abv.bg>
2016-10-15 14:56:39 +0300

    homework02

---
+++ G/02/06/01_int_task01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+int a,a2,a3;
+printf("input a:");
+scanf("%d",&a);
+a2=a*a;
+a3=a*a*a;
+printf("a^2=""%d\n",a2);
+printf("a^3=""%d\n",a3);
+return 0;
+}
+
+
+
