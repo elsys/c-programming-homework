9a1fc5c5841d7eaee97dcbc4f72d47cad87d259b
todorakisa <todorbingov@abv.bg>
2016-10-15 16:22:57 +0300

    Homework

---
+++ B/02/27/27_Homework-task01.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int t=0;
+printf("\nInput Number:");
+scanf("%d",&t);
+int i=t*t;
+int j=t*t*t;
+printf("%d",i);
+printf("\n%d\n",j);
+return 0;
+}
---
+++ B/02/27/27_Homework-task04.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+char n,m;
+printf("Input small english letter:");
+scanf("%c",&n);
+m='z'-(n-'a');
+printf("%c\n",m);
+return 0;
+}
