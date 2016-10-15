c5d93e6ef8172a124187ea732e8ab1649edec26e
root <alexander.petrov420@gmail.com>
2016-10-15 23:05:43 +0300

    eto

---
+++ V/02/03/03_chislo_ime.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+int k;
+scanf("%d",&k);
+        switch(k){
+                case 1:printf("ONE");
+		break;
+                case 2:printf("TWO");
+		break;
+                case 3:printf("THREE");
+		break;
+                case 4:printf("FOUR");
+		break;
+                case 5:printf("FIVE");
+		break;
+                case 6:printf("SIX");
+		break;
+                case 7:printf("SEVEN");
+		break;
+                case 8:printf("EIGHT");
+		break;
+                case 9:printf("NINE");
+		break;
+                default: printf("vuvdeniya simvol ne e cifra");
+		break;
+        }
+return 0;
+}
+
