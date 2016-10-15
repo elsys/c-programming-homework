66e436e7b5f91ed4d157b03a1e3c4ecbb79c7a65
viktorgg <viktor.g.g@abv.bg>
2016-10-15 20:20:09 +0300

    Create task04.c

---
+++ A/02/05/task04.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char a;
+    printf("Enter a letter form the English alphabet:\n");
+    scanf("%c",&a);
+    if(a=='a')printf("z");
+    else if(a=='b')printf("y");
+    else if(a=='c')printf("x");
+    else if(a=='d')printf("w");
+    else if(a=='e')printf("v");
+    else if(a=='f')printf("u");
+    else if(a=='g')printf("t");
+    else if(a=='h')printf("s");
+    else if(a=='i')printf("r");
+    else if(a=='j')printf("q");
+    else if(a=='k')printf("p");
+    else if(a=='l')printf("o");
+    else if(a=='m')printf("n");
+    else if(a=='n')printf("m");
+    else if(a=='o')printf("l");
+    else if(a=='p')printf("k");
+    else if(a=='q')printf("j");
+    else if(a=='r')printf("i");
+    else if(a=='s')printf("h");
+    else if(a=='t')printf("g");
+    else if(a=='u')printf("f");
+    else if(a=='v')printf("e");
+    else if(a=='w')printf("d");
+    else if(a=='x')printf("c");
+    else if(a=='y')printf("b");
+    else if(a=='z')printf("a");
+}
