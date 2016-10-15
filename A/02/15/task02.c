4efbf3068973bb362a98d5d20c608fc18ef87339
MartinPetrovWasTaken <martin033@abv.bg>
2016-10-15 22:30:06 +0300

    Update task02.c

--- A/02/15/task02.c
+++ A/02/15/task02.c
@@ -3,11 +3,9 @@
 int main(){
   int n = 3 , i = 0;
   float a, max;
-    printf("Enter a number:\n");
     scanf("%f", &a);
       max = a;
     do{
-      printf("Enter a number:\n");
       scanf("%f", &a);
 
         if (max < a){
@@ -16,7 +14,7 @@ int main(){
           i++;
     }while(i < n - 1);
 
-    printf("The max of these numbers is:\n%.2f\n", max);
+    printf("\n%.2f\n", max);
 
     return 0;
 }
