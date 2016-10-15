008fa4b087c535242b29e43f00fcc1e44ef63dbb
Katlin Milcheva <kati_mil4eva@abv.bg>
2016-10-15 16:34:08 -0700

    Katlin Milcheva- homework

---
+++ V/02/14/01_Katlin.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    printf("\n%d",n*n);
+    printf("\n%d",n*n*n);
+}
---
+++ V/02/14/04_Katlin.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+char c;
+scanf("%c", &c);
+int k;
+k=c;
+int n;
+n=k-96;
+char a;
+a=123-n;
+printf("%c", a);
+return 0;
+} 
