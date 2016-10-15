5b7ac691e41207dd1c1e92015bf00bebf36faa2c
NikolaNikolow <nikolanikolov837@gmail.com>
2016-10-15 19:46:41 +0300

    Fixed some braces

--- V/02/18/NumbertoText_task03.c
+++ V/02/18/NumbertoText_task03.c
@@ -10,44 +10,33 @@ int main()
         switch(integer)
         {
                 case 0: printf("Zero\n");
-                {
-                        break;
-                }
+	                break;
+
                 case 1: printf("One\n");
-                {
                         break;
-                }
+
                 case 2: printf("Two\n");
-                {
                         break;
-                }
+
                 case 3: printf("Three\n");
-                {
                         break;
-                }
+                
                 case 4: printf("Four\n");
-                {
                         break;
-                }
+
                 case 5: printf("Five\n");
-                {
                         break;
-                }
+
                 case 6: printf("Six\n");
-                {
                         break;
-                }
+
                 case 7: printf("Seven\n");
-                {
                         break;
-                }
+
                 case 8: printf("Eight\n");
-                {
                         break;
-                }
+
                 case 9: printf("Nine\n");
-		{
 			break;
-		}
 	}
 }	
