8d20914fbe1af1dac85199934c79a74ee555de53
qnkee <gr0tato@gmail.com>
2016-10-16 22:13:43 +0300

    Edited without extra printfs

--- V/02/01/02_Qnkee.c
+++ V/02/01/02_Qnkee.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 int main(){
 	float a,b,c;
-	printf("Input 3 numbers");
-	printf("\n Input first number:");
 	scanf("%f", &a);
-	printf("\n Input second number:");
 	scanf("%f", &b);
-	printf("\n Input final number:");
 	scanf("%f", &c);
 	if(a>b && a>c){
 	printf("%.1f\n",a);
@@ -15,7 +11,7 @@ int main(){
 	printf("%.1f\n", b);
 	}
 	else if(c>a && c>b){
-	printf("The biggest number is:%.1f\n", c);
+	printf("%.1f", c);
 	}
 return 0;
 }
