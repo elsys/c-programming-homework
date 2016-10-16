4b48205580955d9ea2373f95024ea6d0940cb8d9
Ivan Viyachki <ivan_viyachki@abv.bg>
2016-10-16 20:30:16 +0300

    Update 09_Zadacha2.c

--- B/02/09/09_Zadacha2.c
+++ B/02/09/09_Zadacha2.c
@@ -1,1 +1,36 @@
+#include<stdio.h>
+int main()
 
+
+
+{
+	float i,l,v;
+	
+	printf("Input i:");
+	scanf("%f",&i);
+	printf("Input l:");
+	scanf("%f",&l);
+	printf("Input v:");
+	scanf("%f",&v);
+	float max=i;
+	
+	if(max<l)
+	{
+		max=l;
+			if(max<v)
+			{
+		        	max=v;
+			}	
+	
+	}
+	
+	else if(max<v)
+	{
+		max=v;
+	}
+	printf("%.3f",max);
+	return 0;
+
+
+
+}
