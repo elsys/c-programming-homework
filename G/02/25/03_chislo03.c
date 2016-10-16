c78adc30c94e09c81f453b3c7cd2054de5091489
Trayan1 <trayan333@gmail.com>
2016-10-16 21:40:12 +0300

    homework

--- G/02/25/03_chislo03.c
+++ G/02/25/03_chislo03.c
@@ -9,7 +9,7 @@ if(a==1){printf("\n one");}
 if(a==2){printf("\n two");}
 if(a==3){printf("\n three");}
 if(a==4){printf("\n four");}
-if(a==5){printf("\n Five");}
+if(a==5){printf("\n five");}
 if(a==6){printf("\n six");}
 if(a==7){printf("\n seven");}
 if(a==8){printf("\n eight");}
