39cce10e2d4f2707abca380a44dd5eba4d147f6e
BorilSarbev <b.sarbev@gmail.com>
2016-10-16 10:57:11 -0700

    Update and rename 02_task01.c to task01.c

--- V/02/05/task01.c
+++ V/02/05/task01.c
@@ -4,14 +4,13 @@ void main(void)
 {
 	int a,sq,cb;
 	
-	printf("\nEnter a number:");
 	scanf("%d",&a);
 
 	sq=a*a;
 	cb=a*a*a;
 
-	printf("\n Square:%d",sq);
-	printf("\n Cube:%d",cb);
+	printf("\n %d",sq);
+	printf("\n %d",cb);
 
 
 }
