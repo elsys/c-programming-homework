d6b11fee2fd333c52f63ac9c167cc9e2ebacb4e6
VKanevv <vesko.kynev@abv.bg>
2016-10-17 00:17:40 +0300

    Update 02_3numbers.c

--- G/02/06/02_3numbers.c
+++ G/02/06/02_3numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(){
-double number1,number2,number3,max;
-scanf("%lf",&number1);
-scanf("%lf",&number2);
-scanf("%lf",&number3);
+float number1,number2,number3,max;
+scanf("%f",&number1);
+scanf("%f",&number2);
+scanf("%f",&number3);
 if(number1<number2){
 max=number2;}else{max=number1;}
 if(max<number3){
 max=number3;}
-printf("%lf\n",max);
+printf("%f\n",max);
 return 0;
 }
 
