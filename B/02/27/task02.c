3890205d1e5753b2ccd4fce1fd7b4b9f3e2ffbe0
todorakisa <todorbingov@abv.bg>
2016-10-16 20:23:02 +0300

    popravka na 2 zad

--- B/02/27/task02.c
+++ B/02/27/task02.c
@@ -6,12 +6,12 @@ scanf("%f",&f1);
 scanf("%f",&f2);
 scanf("%f",&f3);
 if(f1>f2 && f1>f3)
-printf("%.5f\n",f1);
+printf("%.1f\n",f1);
 else if(f2>f1 && f2>f3)
-printf("%.5f\n",f2);
+printf("%.1f\n",f2);
 else if(f3>f1 && f3>f2)
-printf("%.5f\n",f3);
+printf("%.1f\n",f3);
 else if(f2==f3==f1)
-printf("%.5f\n",f1);
+printf("%.1f\n",f1);
 return 0;
 }
