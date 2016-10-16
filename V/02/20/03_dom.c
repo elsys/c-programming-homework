6896d791a847f4d1045478cabc934e214f8b43e1
BistraStefanova <nickothem@gmail.com>
2016-10-16 19:36:42 +0300

    add dom 1-4

---
+++ V/02/20/03_dom.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main() 
+{
+int n;
+scanf("%d", &n);
+switch(n)
+{
+case 0:
+printf("Zero \n");
+break;
+case 1:
+printf("One \n");
+break;
+case 2:
+printf("Two \n");
+break;
+case 3:
+printf("Three \n");
+break;
+case 4:
+printf("Four \n");
+break;
+case 5:
+printf("Five \n");
+break;
+case 6:
+printf("Six \n");
+break;
+case 7:
+printf("Seven \n");
+break;
+case 8:
+printf("Eight \n");
+break;
+case 9:
+printf("Nine \n");
+break;
+}
+return 0;
+}
