54959b226d58d370a99ee449f22917f6205db98a
NikolaNikolow <nikolanikolov837@gmail.com>
2016-10-16 15:25:37 +0300

    Popravka #4

--- V/02/18/SquaresCubes_task01.c
+++ V/02/18/SquaresCubes_task01.c
@@ -2,12 +2,11 @@
 int main()
 {
         int i,sq,cb;
-        printf("\n Enter a nubmer:");
         scanf("%d",&i);
 
         sq=i*i;
         cb=sq*i;
 
-        printf("\n Square of %d is ",sq);
-        printf("\n Cube of %d is\n ",cb);
+        printf("\n%d",sq);
+        printf("\n%d",cb);
 }   
