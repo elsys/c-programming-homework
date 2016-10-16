251537d3017236fe95d419c13fddcd7e9800847e
Trayan1 <trayan333@gmail.com>
2016-10-16 21:37:53 +0300

    homework

--- G/02/25/03_chislo03.c
+++ G/02/25/03_chislo03.c
@@ -2,16 +2,16 @@
 int main()
 {
 int a;
-do{printf("\n Vuvedi cifra");
-scanf("%d",&a);}while(a<0||a>9);
-if(a==0){printf("\n Zero");}
-if(a==1){printf("\n One");}
-if(a==2){printf("\n Two");}
-if(a==3){printf("\n Three");}
-if(a==4){printf("\n Four");}
+printf("\n Vuvedi cifra");
+scanf("%d",&a);
+if(a==0){printf("\n zero");}
+if(a==1){printf("\n one");}
+if(a==2){printf("\n two");}
+if(a==3){printf("\n three");}
+if(a==4){printf("\n four");}
 if(a==5){printf("\n Five");}
-if(a==6){printf("\n Six");}
-if(a==7){printf("\n Seven");}
-if(a==8){printf("\n Eight");}
-if(a==9){printf("\n Nine");}
+if(a==6){printf("\n six");}
+if(a==7){printf("\n seven");}
+if(a==8){printf("\n eight");}
+if(a==9){printf("\n nine");}
 }
