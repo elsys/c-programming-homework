61df44ce6c0c11a33ea403f0fade75edf8c6ee35
Mario Iordanov <marioeqk@gmail.com>
2016-10-16 09:42:59 -0700

    Create 02_domashno.c

---
+++ G/02/18/02_domashno.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() 
+{
+float a,b,c;
+printf("\n Iclude number1:");
+scanf("%f",&a);
+printf("\n Iclude number2:");
+scanf("%f",&b);
+printf("\n Iclude number3:");
+scanf("%f",&c);
+if(c<a)if(b<a)printf("\n %.1f",&a);
+else printf("\n %.1f",&b);
+else if(b<c)printf("\n %.1f",&c);
+else printf("\n %.1f",&b);
+}
