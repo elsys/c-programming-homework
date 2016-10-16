3678a3844f5b1b79ae6eae71c26f0858b9119cb0
Tincty <krisko965@gmail.com>
2016-10-16 13:19:17 +0300

    Update 04_Krisko.c

--- V/02/15/04_Krisko.c
+++ V/02/15/04_Krisko.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 int main()
 {
-	char c,opp;
-	int n,op,k;
-		printf("Insert symbol:\n");
-		scanf("%c", &c);
-		n=c;
-	
+    char c,opp;
+    int n,op,k;
+        scanf("%c", &c);
+        n=c;
         k=  n - 97;
         op = 122 - k;
         opp = op;
-        printf("%c\n", opp);
+        printf("%c", opp);
 return 0;
 }
