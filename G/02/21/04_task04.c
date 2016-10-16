9b684891ea6572d51333742aead7451e3846da21
Mihail Tsolov <misho.tsolov@gmail.com>
2016-10-16 11:17:51 -0700

    Domashno 2

---
+++ G/02/21/04_task04.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char a;
+scanf("%c", &a);
+if(a=='a')
+{printf("\n z");}
+if(a=='b')
+{printf("\n y");}
+if(a=='c')
+{printf("\n x");}
+if(a=='d')
+{printf("\n w");}
+if(a=='e')
+{printf("\n v");}
+if(a=='f')
+{printf("\n u");}
+if(a=='g')
+{printf("\n t");}
+if(a=='h')
+{printf("\n s");}
+if(a=='i')
+{printf("\n r");}
+if(a=='j')
+{printf("\n q");}
+if(a=='k')
+{printf("\n p");}
+if(a=='l')
+{printf("\n o");}
+if(a=='m')
+{printf("\n n");}
+if(a=='n')
+{printf("\n m");}
+if(a=='o')
+{printf("\n l");}
+if(a=='p')
+{printf("\n k");}
+if(a=='q')
+{printf("\n j");}
+if(a=='r')
+{printf("\n i");}
+if(a=='s')
+{printf("\n h");}
+if(a=='t')
+{printf("\n g");}
+if(a=='u')
+{printf("\n f");}
+if(a=='v')
+{printf("\n e");}
+if(a=='w')
+{printf("\n d");}
+if(a=='x')
+{printf("\n c");}
+if(a=='y')
+{printf("\n b");}
+if(a=='z')
+{printf("\n a");}
+return 0;
+}
+
