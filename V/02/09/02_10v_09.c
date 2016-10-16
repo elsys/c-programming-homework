4d9a34c2ea29aa93354022d78c5d2626381b483d
DariNikitova <dnikitova@abv.bg>
2016-10-16 13:36:56 +0300

    Update 02_10v_09.c

--- V/02/09/02_10v_09.c
+++ V/02/09/02_10v_09.c
@@ -9,13 +9,13 @@ int main(){
 
 	if(edno>=dve && edno>=tri)
 	{
-		printf("%f\n", edno);
+		printf("%.1f\n", edno);
 	}else if(dve>=tri && dve>=edno)
 	{
-		printf("%f\n", dve);
+		printf("%.1f\n", dve);
 	}else if(tri>=edno && tri>= dve)
 	{
-		printf("%f\n", tri);
+		printf("%.1f\n", tri);
 	}
 
 	return 0;
