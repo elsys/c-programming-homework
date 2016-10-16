a9a1b76162c69f8dc86c4b9fc9de84b4bbd8bf5c
uncurry <zaharitasev@abv.bg>
2016-10-16 11:56:59 -0700

    Results

---
+++ A/02/10/task01.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a, n, p;
+	printf("Enter value for a: ");
+	scanf("%d",&a);
+		p=a*a;
+		n=a*a*a;
+		printf("%d\n",p);
+		printf("%d\n",n);
+}
---
+++ A/02/10/task04.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	char n, m;
+	printf("Enter a letter: \n");
+	scanf("%c",&n);
+	
+	m = 'z' -(n - 'a');
+
+	printf("\n%c \n", m);
+}
