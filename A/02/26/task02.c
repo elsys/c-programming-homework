44cbe10341aef408a77073a4ab3f6ae8b71dde80
juli00913 <jul.slavchev.tues19@gmail.com>
2016-10-17 20:42:29 +0300

    Update task02.c

--- A/02/26/task02.c
+++ A/02/26/task02.c
@@ -3,16 +3,13 @@
 int main()
 {
     float n, m, p;
-    printf("Input 1st num:\n");
     scanf("%f", &n);
-    printf("Input 2nd num:\n");
     scanf("%f", &m);
-    printf("Input 3rd num:\n");
     scanf("%f", &p);
-    if (n>m && n>p){
+    if (n>m && n>p) {
         printf("%.1f", n);
     }
-    else if (m>n && m>p){
+    else if (m>n && m>p) {
         printf("%.1f", m);
     }
     else {
