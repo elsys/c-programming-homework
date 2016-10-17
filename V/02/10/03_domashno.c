4ed10501afbd15f59a918b9a33ec71ddb95d0a40
Fenrir25 <panda1793@mail.ru>
2016-10-17 09:04:40 -0700

    Domashno

---
+++ V/02/10/03_domashno.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+int main(){
+int x;
+printf("\n ");
+scanf("%d",&x);
+if(x==1){
+printf(" One\n");
+}
+if(x==2){
+printf(" Two\n");
+}
+if(x==3){
+printf(" Three\n");
+}
+if(x==4){
+printf(" Four\n");
+}
+if(x==5){
+printf(" Five\n");
+}
+if(x==6){
+printf(" Six\n");
+}
+if(x==7){
+printf(" Seven\n");
+}
+if(x==8){
+printf(" Eight\n");
+}
+if(x==9){
+printf(" Nine\n");
+}
+return 0;
+}
