7a6516d3575c5b1ff2fdee97feb2168173348c78
juli00913 <jul.slavchev.tues19@gmail.com>
2016-10-17 20:12:30 +0300

    Create task01.c

---
+++ A/02/26/task01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, m, p;
+
+    printf("Input number:");
+    scanf("%d", &n);
+    m=n*n;
+    p=n*m;
+    printf("%d" ,m);
+    printf("\n%d" ,p);
+
+    return 0;
+}
