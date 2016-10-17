baf71048174d2b6490c479f540b33af26c871fc1
I-Mircheva <ivona1mircheva@gmail.com>
2016-10-17 20:49:06 +0300

    Homework Nr02 from Ivona Mircheva 10b class 10Nr

---
+++ B/02/10/03_english_numb.c
@@ -0,0 +1,53 @@
+
+
+
+
+#include "stdio.h"
+
+
+void main() {
+
+  int n = 0;
+
+switch(n){
+
+    case 1:
+    printf("one");
+    break;
+    case 2:
+    printf("two");
+    break;
+    case 3:
+    printf("three");
+    break;
+    case 4:
+    printf("four");
+    break;
+    case 5:
+    printf("five");
+    break;
+    case 6:
+    printf("six");
+    break;
+    case 7:
+    printf("seven");
+    break;
+    case 8:
+    printf("eight");
+    break;
+    case 9:
+    printf("nine");
+    break;
+    case 0:
+    printf("zero");
+    break;
+  default:
+ printf("wrong");
+    break;
+
+
+}
+
+
+
+}
