0fe1d8274fb1de111c48d50d1d2e46a7e30696fe
DonViktor <victor.popov00@gmail.com>
2016-10-17 23:18:37 +0300

    hello

---
+++ V/02/07/04_bukvi.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	char s;
+        printf("\n Vuvedi bukva:);
+	scanf("%c",&s);
+	
+	if(s=='a'){
+	printf("\n z");
+	}
+	if(s=='b'){
+	printf("\n y");
+	}
+	if(s=='c'){
+	printf("\n x");
+	}
+	if(s=='d'){
+	printf("\n w");
+	}
+	if(s=='e'){
+	printf("\n v");
+	}
+	if(s=='f'){
+	printf("\n u");
+	}
+	if(s=='g'){
+	printf("\n t");
+	}
+	if(s=='h'){
+	printf("\n s");
+	}
+	if(s=='i'){
+	printf("\n r");
+	}
+	if(s=='j'){
+	printf("\n q");
+	}
+	if(s=='k'){
+	printf("\n r");
+	}
+	if(s=='l'){
+	printf("\n o");
+	}
+	if(s=='m'){
+	printf("\n n");
+	}
+	if(s=='n'){
+	printf("\n m");
+	}
+	if(s=='o'){
+	printf("\n l");
+	}
+	if(s=='p'){
+	printf("\n k");
+	}
+	if(s=='q'){
+	printf("\n j");
+	}
+	if(s=='r'){
+	printf("\n i");
+	}
+	if(s=='s'){
+	printf("\n h");
+	}
+	if(s=='t'){
+	printf("\n g");
+	}
+	if(s=='u'){
+	printf("\n f");
+	}
+	if(s=='v'){
+	printf("\n e");
+	}
+	if(s=='w'){
+	printf("\n d");
+	}
+	if(s=='x'){
+	printf("\n c");
+	}
+	if(s=='y'){
+	printf("\n b");
+	}
+	if(s=='z'){
+	printf("\n a");
+	}
+
+return 0;
+
+}
