04038792c3ad9eec7c14e4fe2cdbc76a546c29b4
kris-lalev <kristian.lalev@gmail.com>
2016-10-17 22:08:52 +0300

    Domashno 02

---
+++ V/02/16/01_SquaresCubes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num,squ,cube;
+    scanf("%d",&num);
+    squ = num*num;
+    cube = num*num*num;
+    printf("\n%d",squ);
+    printf("\n%d",cube);
+    return 0;
+} 
