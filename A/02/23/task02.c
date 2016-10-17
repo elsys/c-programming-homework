ee0f09730383e73ea967ca9347dd581bfe749f9f
staso <stasor12@gmail.com>
2016-10-18 00:08:16 +0300

    Task02 corection

--- A/02/23/task02.c
+++ A/02/23/task02.c
@@ -4,8 +4,8 @@ int main() {
 	float a,b,c;
 	scanf("%f%f%f",&a,&b,&c);
 	if(a>b){
-	if(a>c)printf("%f",a);
-	else printf("%f",c);
-	}else if(b>c)printf("%f",b);
-	else printf("%f",c);
+	if(a>c)printf("%.1f",a);
+	else printf("%.1f",c);
+	}else if(b>c)printf("%.1f",b);
+	else printf("%.1f",c);
 }
