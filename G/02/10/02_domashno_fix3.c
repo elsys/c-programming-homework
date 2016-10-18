35ac1ad4178addaf5004d2d4eb29072c4a867ee6
ErikaIvanova <erikaivanova5@gmail.com>
2016-10-18 10:50:55 +0300

    Create 02_domashno_fix3.c

---
+++ G/02/10/02_domashno_fix3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float a,b,c,max;
+    scanf("%f",&a);
+    scanf("%f",&b);
+    scanf("%f",&c);
+    max=a;
+    if(b>a)max=b;
+    if(c>max)max=c;
+    printf("%.1f \n",max);
+    return 0;
+}
