7836416cfb0d086261691a1b3d6075270ce7b6ee
rrhubenov <rrhubenov@gmail.com>
2016-10-18 20:53:59 +0300

    Update task02.c

--- A/02/22/task02.c
+++ A/02/22/task02.c
@@ -2,11 +2,8 @@
 
 int main() {
   float a,b,c;
-  printf("Give first num:\n");
   scanf("%f", &a);
-  printf("Give second num:\n");
   scanf("%f", &b);
-  printf("Give third num:\n");
   scanf("%f", &c);
   if(a>b){
     if(a>c)printf("The largest num is %f", a);
