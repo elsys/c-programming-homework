f3070b655a7c89af73887c218d98f67032f42c43
repitile <cecoisie@abv.bg>
2016-10-18 23:40:26 +0300

    Update 02_task2.c

--- G/02/22/02_task2.c
+++ G/02/22/02_task2.c
@@ -1,19 +1,16 @@
+#include<stdio.h>
 int main()
 {
     float a,b,c,max;
     max=0;
-printf("\nvuvedi 3 chisla:");
-printf("\n1:");
-scanf("%f", &a);
-printf("\n2:");
-scanf("%f", &b);
-printf("\n3:");
-scanf("%f", &c);
+scanf("%f\n", &a);
+scanf("%f\n", &b);
+scanf("%f\n", &c);
     if(a>max)
         max=a;
     if(b>max)
         max=b;
     if(c>max)
         max=c;
-    printf("%.2f", max);
+    printf("%.1f", max);
 }
