4098ba5ae83343abb65254259496974c1bf77d85
toniuyt123 <antoniomilev123@gmail.com>
2016-10-21 13:39:49 +0300

    last try

---
+++ B/03/02/03_Money.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, k, disc;
+	float m, sum;
+	scanf("%d",&n);
+	scanf("%f",&m);
+	scanf("%d",&k);
+	
+	for(int i = 1;i <= n;i++)
+	{
+		disc = i / k;
+		sum += m - ((m*(disc*2)) / 100);
+	}
+	
+	printf("%.f",sum);
+	
+	return 0;
+}
