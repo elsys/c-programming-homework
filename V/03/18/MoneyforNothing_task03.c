476c5c7545efb2e0d281466b7cd4b6fcdf63f23b
NikolaNikolow <nikolanikolov837@gmail.com>
2016-10-22 17:46:45 +0300

    Modified 4

--- V/03/18/MoneyforNothing_task03.c
+++ V/03/18/MoneyforNothing_task03.c
@@ -2,20 +2,18 @@
 
 int main()
 {
-        int n,m,k,discount=0,money,i,sum=0;
-        scanf("%d",&n);
-	scanf("%d",&m);
-	scanf("%d",&k);
-        money=m;
-        while(i<n)
-        {
-                if(i!=0 && i%k==0 )
-                {
-                        discount=discount+2;
-                        m=money-(money*discount/100);
-                }
-                sum=sum+m;
-                i++;
-        }
-        printf("\n%d",sum);
+	int n, m, k, discount=0, money, i, sum=0;
+	scanf("%d\n%d\n%d",&n,&m,&k);
+	money=m;
+	while(i<n)
+	{
+		if(i!=0 && i%k==0 )
+		{
+			discount=discount+2;
+			m=money-(money*discount/100);
+		}
+		sum=sum+m;
+		i++;
+	}
+	printf("\n%d",sum);
 }
