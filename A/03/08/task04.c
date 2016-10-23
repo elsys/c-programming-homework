2f2206ef9a6219d4ec3d7e908aeaf32c33da1b97
DanyYanev <yanev89@gmail.com>
2016-10-23 12:15:56 -0700

    all done

--- A/03/08/task04.c
+++ A/03/08/task04.c
@@ -4,6 +4,14 @@ int main (){
 	int numoflines, count = 0;
 	scanf("%d", &numoflines);
 	int i;
+	for(i = 0; i <= numoflines/2; i++){
+		if(i != numoflines/2){
+			for (int s = 1; s <= i*2 - 1; s++){
+				count++;
+			}
+		}
+	}
+	printf("%d\n", count);
 	for( i = 0; i <= numoflines/2; i++){ // lines
 
 		for (int j = 0; j < numoflines/2 - i; j++){ // spaces
