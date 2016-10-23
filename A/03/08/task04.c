c311382b3c06b3df2bbcd2568f1c3ff0cfcb0cec
DanyYanev <yanev89@gmail.com>
2016-10-23 12:29:43 -0700

    finals hopes

--- A/03/08/task04.c
+++ A/03/08/task04.c
@@ -4,29 +4,30 @@ int main (){
 	int numoflines, count = 0;
 	scanf("%d", &numoflines);
 	int i;
-	for(i = 0; i <= numoflines/2; i++){
-		if(i != numoflines/2){
+	numoflines--;
+	for(i = 0; i <= numoflines; i++){
+		if(i != numoflines){
 			for (int s = 1; s <= i*2 - 1; s++){
 				count++;
 			}
 		}
 	}
 	printf("%d\n", count);
-	for( i = 0; i <= numoflines/2; i++){ // lines
+	for( i = 0; i < numoflines; i++){ // lines
 
-		for (int j = 0; j < numoflines/2 - i; j++){ // spaces
+		for (int j = 0; j < numoflines - i; j++){ // spaces
 			printf(" ");
 			}
-		if (i != numoflines/2){ //prints *&#
+		if (i != numoflines){ //prints *&#
 			if (i != 0) printf("*");
 			for (int s = 1; s <= i*2 - 1; s++){
 				printf("#");
-				count++;
 			}
 			printf("*");
 		}
-		if(i == numoflines/2){ //last line8
-			for (int i = 0; i < numoflines; i++){
+		if(i == numoflines - 1){ //last line8
+			printf("\n");
+			for (int i = 0; i < numoflines*2 + 1; i++){
 				printf("*");
 			}
 		}
