2a15fdcf3a51f0d7988e2b5cc0bfc8c241976d4b
Nikolai Mironov <nikolai.mironov.1@gmail.com>
2016-10-24 20:00:19 +0300

    update of 01_SumAndAverage

--- V/03/19/01_SumAndAverage.c
+++ V/03/19/01_SumAndAverage.c
@@ -4,17 +4,16 @@ int main()
 {
 	int num, i;
 	float n, avg, sum;
-	sum = 0;
-	i = 1;
 	scanf("%d", &num);
-	do{
+	avg = 0;
+	sum = 0;
+	i = 0;
+	while(i < num){
 		scanf("%f", &n);
-		sum = sum +n;
+		sum = sum + n;
 		i++;
-	} while (i <= num);
-	
-	avg = sum/num;
-	
-	printf("Sum:%.0f\nAvg: %.2f",sum , avg);
+	}
+	avg = sum / num;
+	printf("Sum: %.0f\nAvg: %.2f", sum, avg);
 	return 0;
 }
