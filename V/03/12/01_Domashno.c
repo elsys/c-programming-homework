e1e5aaa90f1a0fdc2603504c62a1859b4f9b14db
Asen Lekov <asenlekoff@gmail.com>
2016-10-24 13:36:46 +0300

    Fixed #718

--- V/03/12/01_Domashno.c
+++ V/03/12/01_Domashno.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
+
 int main()
 {
 float a,b,c=0,i=0,d;
 scanf("%f",&a);
 while(i<a){
-scanf("%f",&b);
-c=c+b;
-i++;
+    scanf("%f",&b);
+    c=c+b;
+    i++;
 }
 d=c/a;
 printf("\nSum: %.f",c);
