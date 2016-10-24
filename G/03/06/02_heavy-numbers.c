a4913036525474a0dfbda99aa9c08be4ff9751fd
VKanevv <vesko.kynev@abv.bg>
2016-10-24 23:39:58 +0300

    Update 02_heavy-numbers.c

--- G/03/06/02_heavy-numbers.c
+++ G/03/06/02_heavy-numbers.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-int a,i;
-float avr;
-scanf("%d",&a);
-	
-while(a>0){
+#include<stdio.h>
+#include<stdlib.h>
+
+int main (){
+  int a, avr, i, b;
+  scanf("%d", &a);
 avr=0;
-i=0;
-a=a/10;
-avr+=(a%10);
-i=i+1;
+  for (i = 0; a!=0; i++){
+    b=a%10;
+    avr=avr+b;
+    a=a/10;
+  }
+  avr=avr/i;
+ if(avr < 7){
+    printf("light\n");}
+ else {
+    printf("heavy\n");}
+  return 0;
 }
-avr=avr/i;
-if(avr<7){
-printf("light\n");}
-if(avr>7){
-printf("heavy\n");}
-return 0;
-}
+
