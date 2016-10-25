8c427f6e63ae111ef7a5f3988927924f05cb1556
puka89 <kristiqn.stoimenov.tues@gmail.com>
2016-10-25 23:44:54 +0300

    14

---
+++ B/02/14/01_lol.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int a,q,w;
+
+        scanf("%d",&a);
+        q = a*a;
+        w = a*a*a;
+        printf("%d\n",q);
+        printf("%d\n",w);
+        return 0;
+}
