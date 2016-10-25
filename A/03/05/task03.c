f85633f67501d47c81157975779363349d544e37
viktorgg <viktor.g.g@abv.bg>
2016-10-25 21:31:35 +0300

    Create task03.c

---
+++ A/03/05/task03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int n,m,k,i=1,sum=0;
+	float r=0.02;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	scanf("%d",&k);
+	for(r*m;i<=n;i++){
+		sum=sum+m;
+		if(n%k==0){
+        m=m-r;
+		}
+	}
+	printf("%d \n",sum);
+}
