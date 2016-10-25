3ee8cf99a2355dafe88750207f86ae3e93255245
puka89 <kristiqn.stoimenov.tues@gmail.com>
2016-10-25 23:05:46 +0300

    14

---
+++ B/02/14/01.c.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+        int a,q,w;
+
+        scanf("%d", &a);
+        q = a*a;
+        w = a*a*a;
+        printf("%d\n", q);
+        printf("%d", w);
+        return 0;
+}
