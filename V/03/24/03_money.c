62fb8e7fbe69a0c7f281725e7d7cb014ee382d0d
TUESwarrior <stefanbabukov98@gmail.com>
2016-10-26 20:49:49 +0300

    Update 03_money.c

--- V/03/24/03_money.c
+++ V/03/24/03_money.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main(){
-	int i,i2,number,price,k,total,price2,discount=100;
+	int i,number,price,k,total,price2,discount=100;
 	scanf("%d",&number);
 	scanf("%d",&price);
 	scanf("%d",&k);
@@ -11,13 +11,11 @@ int main(){
 	while(number!=0){
 		discount=discount-2;
 		price2=price*discount/100;
-		for(i2=0;i2<k;i2++){
+		for(i=0;i<k;i++){
 			total=total+price2;
 		}
 		number=number-k;
 	}
 	printf("%d\n",total);
-
-
-	
+	return 0;
 }
