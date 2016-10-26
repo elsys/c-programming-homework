39132f9f03414d30c27f13f9e2f890c96d83c5ac
DanielRusinov <daniel.rusinov00@gmail.com>
2016-10-26 10:33:19 -0700

    kachvam

---
+++ B/03/06/task02.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,i;
+    float sum,avr;
+
+    sum=0;
+    avr=0;
+
+    scanf("%d",&a);
+
+    while(a>=1 && a<=9999){
+        for(i=0; a>=1; i++){
+           sum = sum + a%10;
+           a=a/10;
+           avr = sum;
+        }
+      if(avr = sum/i>7)	printf("heavy");
+      else printf("light");
+    };
+
+
+    return 0;
+}
