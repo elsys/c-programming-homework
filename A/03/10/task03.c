c6690b35b56bfb53681bbc540ee8e2aefc4b46f8
uncurry <zarcho65@gmail.com>
2016-10-26 09:24:16 +0300

    Correct Task 3

--- A/03/10/task03.c
+++ A/03/10/task03.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 int main()
 {
-	int n, k;
-	float m=0;
+	int n,m,k;
+	
 	int discount=0;
-	float sum=0;
+	int sum=0;
 	scanf("%d",&n);
-	scanf("%f",&m);
+	scanf("%d",&m);
 	scanf("%d",&k);
 	if(k>=n){
 	sum=n*m;
@@ -18,5 +18,5 @@ int main()
 			discount=discount+2;	
 		}
 	}
-	printf("%.2f",sum);
+	printf("%d",sum);
 }
