b8bcf1ba52a4937240f9c3c9fbd6943039c7b85f
repitile <cecoisie@abv.bg>
2016-10-27 14:38:03 +0300

    Create 02_domashnmo.c (#834)

---
+++ G/03/22/02_domashnmo.c
@@ -0,0 +1,26 @@
+
+#include <stdio.h>
+
+int main()
+{
+int a,b,c,i;
+float d;
+d=0;
+i=0;
+scanf("%d",&a);
+b=a;
+while(b>0){
+    c=b%10;
+    b=b/10;
+    d=d+c;
+    i++;
+}
+d=d/i;
+if(d>7){
+    printf("heavy");
+}
+else{
+    printf("light");
+}
+    return 0;
+}
