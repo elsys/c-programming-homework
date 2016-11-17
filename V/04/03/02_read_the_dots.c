00e0d56e56b7e320c0e99d61d796ac478575b276
kocetomad <alexanderpetrov420@gmail.com>
2016-11-17 19:19:32 +0200

    po-burza 2ra 2ri opit

--- V/04/03/02_read_the_dots.c
+++ V/04/03/02_read_the_dots.c
@@ -1,6 +1,6 @@
 #include<stdio.h>                           
 int main(){                                 
-int n,k=0,o,c;                                                                  
+int n,k=0,c;                                                                  
 scanf("%d",&n);
 int arr[n];                             
 	do{                                 
@@ -9,8 +9,7 @@ int arr[n];
 	k++;
 	}while(k<n);
 	for(k=0,c=n-1;k<n/2;k++,c--){
-	o=arr[k]-arr[c];
-	printf("%d\n",o);
+	printf("%d\n",arr[k]-arr[c]);
 	}
 	if(n%2!=0){
 	printf("%d\n",arr[n/2]);
