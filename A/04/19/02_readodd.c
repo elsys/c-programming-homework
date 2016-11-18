1299364307c6988d5d28ed1c8af44f5275b56d4a
Momchil <mom.todorov.tues19@gmail.com>
2016-11-18 23:31:22 +0200

    Fix it pls

--- A/04/19/02_readodd.c
+++ A/04/19/02_readodd.c
@@ -2,20 +2,21 @@
 
 int main()
 {
-	int n, i=0, a[100], hp=0;
+	int n, i=0, a[100];
 	scanf("%d", &n);
 	do
 	{
 		scanf("%d", &a[i]);
 		if(a[i]%2)i++;
 	}while(i!=n);
-	i--;
+	i=0;
+	n--;
 	do
 	{
-		printf("\n%d", a[hp]-a[i]);
-		hp++;
-		i--;
-		if(i==hp)printf("\n%d", a[i]);
-	}while(hp>i);
+		printf("\n%d", a[i]-a[n]);
+		i++;
+		n--;
+		if(i==n)printf("\n%d", a[n]);
+	}while(n>i);
 	return 0;
 }
