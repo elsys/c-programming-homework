917caca544a7729f9101dca70867e0c74ec38792
Daniel Rusinov <daniel.rusinov00@gmail.com>
2016-11-19 00:30:01 +0200

    Update task04.c

--- B/04/06/task04.c
+++ B/04/06/task04.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 int main ()
 {
-	float arr[1000],a,vsicko=0;
-	int i=0,j,b=0;
+        float arr[1000],a,vsicko=0;
+        int i,j,b;
+        i=0,b=0;
+	
+        while(scanf("%f",&a)!=EOF){
+                j=0;
+                for(i=0;i<64;i++){
+                        if(arr[i]==a)j=1;}
 
-	while(scanf("%f",&a)!=EOF){
-		j=0;
-		for(i=0;i<64;i++){
-			if(arr[i]==a)j=1;
-		
-		if(j==0){
-			arr[b]=a;
-			b++;}
-		i++;	
-	}
-	
-	printf("%f \n",a);
+                if(j==0){
+                        arr[b]=a;
+                        b++;}
+                i++;
+        }
 
-	for(i=0;i<b;i++){
-		while(arr[i]>=0){
-			arr[i] = arr[i]-b;}
-	
-		vsicko=vsicko+(arr[i]+b);
-	}
-	
-}
-	printf("%.3f",vsicko);
+        printf("%d\n",b);
+
+        for(i=0;i<b;i++){
+                do{
+                        arr[i] = arr[i]-b;
+                }while(arr[i]>=0);
+
+                vsicko=vsicko+(arr[i]+b);
+        }
+
+        printf("%.3f",vsicko);
 
 return 0;
 }
+
