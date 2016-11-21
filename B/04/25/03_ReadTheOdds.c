7efb720d9a6ea0006569aabb4b45c4cf98fb31cb
Спас Зидаров <zidarovspas@gmail.com>
2016-11-21 22:27:24 +0200

    I'm sorry I'm late. (#1026)
    * Sorry I'm late.
    
    * Update 01_Frequency.c
    
    * Update 02_ReverseIt.c
    
    * Update 02_ReverseIt.c
    
    * Update 02_ReverseIt.c
    
    * Update 01_Frequency.c
    
    * Update 01_Frequency.c
    
    * Update 02_ReverseIt.c
    
    * Update 01_Frequency.c
    
    * Update 03_ReadTheOdds.c

---
+++ B/04/25/03_ReadTheOdds.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char snts[1000];
+	int alph[26]={0},i=0;	
+	fgets(snts,1000,stdin);
+	while(snts[i]!='\0') 
+	{
+	        if(snts[i]>='A'&&snts[i]<='Z') 
+		{
+        		snts[i]=snts[i]+32;
+        	}
+       		i++;
+	}
+	i=0;
+	while(snts[i]!='\0')
+	{
+	        if(snts[i]>='a'&&snts[i]<='z')
+		{
+	            alph[snts[i]-'a']++;
+	        }
+        i++;
+	}
+	for(i=0;i<26;i++)
+	{
+        if(alph[i]!=0)
+	{
+            printf("%c - %d\n",i+'a',alph[i]);
+        }
+    }
+    return 0;
+}
