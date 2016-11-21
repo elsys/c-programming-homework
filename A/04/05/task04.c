cd691b70b70c931f076a29d94d6aac319c973876
viktorgg <viktor.g.g@abv.bg>
2016-11-21 22:48:23 +0200

    Update task04.c (#932)
    * Update task04.c
    
    * Update task04.c
    
    * Update task04.c
    
    * Update task04.c

--- A/04/05/task04.c
+++ A/04/05/task04.c
@@ -2,25 +2,23 @@
 #include<stdlib.h>
 #include<math.h>
 
-int main()
-{
-	float array[63],j;
-	float sum=0;
-	int i=0,broi=0,f=1;
+int main(){
 
-	while(scanf("%f",&j)!=EOF){
-    do{
+	float array[63]; 
+	float sum=0,j;
+	int i,broi=0,f=1,r=1;
+	
+	while(scanf("%f",&j)!=EOF)
+        {
+		for(i=0; i<broi; i++)
         if(j==array[i])
         f=0;
-        if(f>0){
-        array[broi]=j;
-        broi=broi+1;
-        }
-        f=1; i=i+1;
-    }while(i<broi);
-	}
-	for(i=0;i<broi;i++){
-    sum+=fmod(array[i],broi);
-	printf("%d\n%.3f", broi, sum);
-	}
+		if(f)
+		{
+        array[broi] = j;broi=broi+1;
+		}f = r;
+	    }
+	for(i=0;i<broi;i++)
+		sum=sum+fmod(array[i],broi);
+	printf("%d\n%.3f",broi,sum);
 }
